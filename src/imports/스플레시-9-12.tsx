1315b38718d3b9c10b0fc8bddd5645b90969a683
birdSR <birdnewmind94@gmail.com>
2025-11-28 13:01:54 +0900

    commit

--- src/imports/스플레시-9-12.tsx
+++
@@ -1,9 +0,0 @@
-export default function Component() {
-  return (
-    <div className="relative size-full" data-name="스플레시">
-      <video autoPlay className="absolute max-w-none object-cover size-full" controlsList="nodownload" loop playsInline>
-        <source src="/_videos/v1/675fdc8b02cce42e07815828fbc84177a0857b4b" />
-      </video>
-    </div>
-  );
-}
